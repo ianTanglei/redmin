328baa74f280d8b057748ba0a40d412f560c22ec
lotus-tanglei <tanglei_12301@163.com>
2019-12-18 22:04:09 +0800

    项目创建

---
+++ redmin_controller/src/main/java/com/lotus/controller/RedminControllerApplication.java
@@ -0,0 +1,13 @@
+package com.lotus.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedminControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedminControllerApplication.class, args);
+    }
+
+}
